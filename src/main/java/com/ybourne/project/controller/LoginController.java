20475b87470db5f9bb5f1aa7e371a976dfa475ef
xueyb <49144395+yBourne@users.noreply.github.com>
2021-04-30 08:36:37 +0800

    test commit develop

--- src/main/java/com/ybourne/project/controller/LoginController.java
+++ src/main/java/com/ybourne/project/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.ybourne.project.controller;
 
 public class LoginController {
-
+    // 现在我再在develop里做一点改动
+    public static void main(String[] args){
+        System.out.println("hello?");
+    }
 }
